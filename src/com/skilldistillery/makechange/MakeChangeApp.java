f467d6c6bfb2ae55f734346410b2a7f2ed75e1b8
Alex Murphy <alexander.f.murphy@gmail.com>
2018-04-28 23:43:04 -0600

    pennies fixed, working program

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -6,35 +6,43 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 
-		double tester = 0, diff = 0, temp = 0;
-		double price = itemPrice();
-		double tendered = amountTendered();
-		diff = tendered - price;
+		double temp = 0;
+		float tester = 0f; 
+		int diff = 0;
+		float price = (float)itemPrice();
+		float tendered = (float)amountTendered();
+		diff = (int)( tendered - price);
+		tester = (float)(tendered - price);
+//		System.out.println(tester);
+//		System.out.println(diff);
+		
 		
 		String firstResponse = priceDifference(diff);
 		System.out.println(firstResponse);
 
-		while(diff > 0) {
+		while (diff > 0){
 		System.out.println("Tens: " + ten(diff));
-		diff = diff - ten(diff) * 10;
+		diff = diff - ten(diff) * 1000;
 		
 		System.out.println("Fives: " + five(diff));
-		diff = diff - five(diff) * 5;
+		diff = diff - five(diff) * 500;
 		
 		System.out.println("Dollars: " + dollar(diff));
-		diff = diff - dollar(diff) * 1;
+		diff = diff - dollar(diff) * 100;
 		
 		System.out.println("Quarters: " + quarter(diff));
-		diff = diff - quarter(diff) * 0.25;
+		diff = diff - quarter(diff) * 25;
 		
 		System.out.println("Dimes: " + dime(diff));
-		diff = diff - dime(diff) * 0.1;
+		diff = diff - dime(diff) * 10;
 		
-		System.out.println("Nickles: " + nickle(diff));
-		diff = diff - nickle(diff) * 0.05;
+		System.out.println("Nickels: " + nickel(diff));
+		diff = diff - nickel(diff) * 5;
 		
 		System.out.println("Pennies: " + penny(diff));
-		diff = diff - penny(diff) * 0.01;
+		diff = diff - penny(diff) * 1;
+		
+		diff = 0;
 		}
 
 		// int[] testArray = { 1, 2, 3, 4, 5, 6 };
@@ -46,7 +54,7 @@ public class MakeChangeApp {
 
 	}
 
-	public static String priceDifference(double diff) {
+	public static String priceDifference(int diff) {
 		double difference = diff;
 		String response;
 		if (difference < 0) {
@@ -61,68 +69,68 @@ public class MakeChangeApp {
 		}
 	}
 
-	public static double itemPrice() {
+	public static int itemPrice() {
 		System.out.print("Please enter the item price: ");
 		Scanner input = new Scanner(System.in);
-		double price = input.nextDouble();
+		int price = (int) (100 *(input.nextDouble()));
 		return price;
 	}
 
-	public static double amountTendered() {
+	public static int amountTendered() {
 		System.out.print("Please enter the amount tendered: ");
 		Scanner input = new Scanner(System.in);
-		double tendered = input.nextDouble();
+		int tendered = (int)(100 *(input.nextDouble()));
 		return tendered;
 	}
 
-	public static int ten(double diff) {
-		double temp = diff % 10;
-		int ten = (int) ((diff - temp) / 10);
-		diff = diff - (ten * 10);
+	public static int ten(int diff) {
+		double temp = diff % 1000;
+		int ten = (int) ((diff - temp) / 1000);
+		diff = diff - (ten * 1000);
 		return ten;
 	}
 
-	public static int five(double diff) {
-		double temp = diff % 5;
-		int five = (int) ((diff - temp) / 5);
-		diff = diff - (five * 5);
+	public static int five(int diff) {
+		double temp = diff % 500;
+		int five = (int) ((diff - temp) / 500);
+		diff = diff - (five * 500);
 		return five;
 	}
 
-	public static int dollar(double diff) {
-		double temp = diff % 1;
-		int dollar = (int) ((diff - temp) / 1);
-		diff = diff - (dollar * 1);
+	public static int dollar(int diff) {
+		double temp = diff % 100;
+		int dollar = (int) ((diff - temp) / 100);
+		diff = diff - (dollar * 100);
 		return dollar;
 	}
 
-	public static int quarter(double diff) {
-		double temp = diff % 0.25;
-		int quarter = (int) ((diff - temp) / 0.25);
-		diff = diff - (quarter * 0.25);
+	public static int quarter(int diff) {
+		double temp = diff % 25;
+		int quarter = (int) ((diff - temp) / 25);
+		diff = diff - (quarter * 25);
 		return quarter;
 	}
 
-	public static int dime(double diff) {
-		double temp = diff % 0.1;
-		int dime = (int) ((diff - temp) / 0.1);
-		diff = diff - (dime * 0.1);
+	public static int dime(int diff) {
+		double temp = diff % 10;
+		int dime = (int) ((diff - temp) / 10);
+		diff = diff - (dime * 10);
 		return dime;
 	}
 
-	public static int nickle(double diff) {
-		double temp = diff % 0.05;
-		int nickle = (int) ((diff - temp) / 0.05);
-		diff = diff - (nickle * 0.05);
-		return nickle;
+	public static int nickel(int diff) {
+		double temp = diff % 5;
+		int nickel = (int) ((diff - temp) / 5);
+		diff = diff - (nickel * 5);
+		return nickel;
 	}
 
-	public static int penny(double diff) {
-		double temp = diff % 0.01;
-		
-		int penny = (int) (1 + ((diff - temp) / 0.01));
-		diff = diff - (penny * 0.01);
+	public static int penny(int diff) {
+		double temp = diff % 1;
+		int penny = (int) ((diff - temp) / 1);
+		diff = diff - (penny * 1);
 		return penny;
+		
 	}
 
 	public static void arrayTest(int[] array) {
